8a05f80f9ddbcb98ab8d7835ef6e7d070786523f
Imad Kazi <imad@Imads-MacBook-Pro.local>
2022-06-22 19:11:24 +0100

    imadkazi - react init

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-slate-200">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h3 className='text-slate-800'>
+          Website in development
+        </h3>
+        
       </header>
     </div>
   );
