a916cb83c43ec67b3631743a6f2710d490c07975
Imad Kazi <imad@Imads-MacBook-Pro.local>
2022-06-23 11:08:29 +0100

    imadkazi - adding logo and contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import logo from './Assets/logo.svg';
+import './styles.scss'
 
 function App() {
   return (
     <div className="bg-slate-200">
       <header className="App-header">
-        
-        <h3 className='text-slate-800'>
+        <div className='flex justify-center items-center flex-col mb-5'>
+          <img src={logo} className="mb-3" width={250} alt="logo" />
+          <p className='text__role text-slate-600 uppercase font-regular'>Designer | Developer | Photographer</p>
+        </div>
+        <h3 className='text-slate-900'>
           Website in development
         </h3>
+        <h4 className='text-slate-900 text__email font-medium'>
+          For enquiries please email hello@imadkazi.co.uk
+        </h4>
         
       </header>
     </div>
