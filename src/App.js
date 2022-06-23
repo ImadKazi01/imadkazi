9c8166315486b737234f71e3b8c1762c6c1a24c0
Imad Kazi <imad@Imads-MacBook-Pro.local>
2022-06-23 11:13:21 +0100

    imadkazi - adding logo and contact styling changes

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
       <header className="App-header">
         <div className='flex justify-center items-center flex-col mb-5'>
           <img src={logo} className="mb-3" width={250} alt="logo" />
-          <p className='text__role text-slate-600 uppercase font-regular'>Designer | Developer | Photographer</p>
+          <p className='text-sm text-slate-600 uppercase font-medium'>Designer | Developer | Photographer</p>
         </div>
         <h3 className='text-slate-900'>
           Website in development
         </h3>
-        <h4 className='text-slate-900 text__email font-medium'>
+        <h4 className='text-slate-900 font-medium text-center px-5 my-2'>
           For enquiries please email hello@imadkazi.co.uk
         </h4>
         
