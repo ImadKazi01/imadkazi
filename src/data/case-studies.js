5ea799ec8e81bada1797d7028595dc718569f771
Imad <hello@imadkazi.co.uk>
2025-11-21 18:29:18 +0000

    Case study: Added Lunara

--- src/data/case-studies.js
+++ src/data/case-studies.js
@@ -2,5 +2,6 @@ import burgerize from './case-studies/burgerize.json'
 import cleverbee from './case-studies/cleverbee.json'
 import littleGreenVan from './case-studies/little-green-van.json'
 import dolceJuices from './case-studies/dolce-juices.json'
+import lunara from './case-studies/lunara.json'
 
-export default [burgerize, cleverbee, littleGreenVan, dolceJuices]
+export default [burgerize, cleverbee, littleGreenVan, dolceJuices, lunara]
